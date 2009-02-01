6cf292ef0e4b964020185cbac10226a86c5ec882
T_Y_Z@mail.ru <T_Y_Z@mail.ru@45cc0cd2-ed74-11dd-b00f-9fabb4b2ff13>
2009-02-01 11:25:19 +0000

    [*] Структурирование кода [+] Для показа цитат используется компонент Thtml [+] Изменения в TestTab. Лог, консоль ввода, многовкладочность.

--- Sources/Main.pas
+++ Sources/Main.pas
@@ -2,10 +2,13 @@ unit Main;
 
 interface
 
+// Проверить, все ли используются.
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, ComCtrls, cxClasses, dxNavBarBase, dxNavBarCollns, ExtCtrls,
-  dxNavBar, StdCtrls;
+  dxNavBar, StdCtrls, IdBaseComponent, IdComponent, IdTCPConnection,
+  IdTCPClient, IdHTTP, IdAntiFreezeBase, IdAntiFreeze, OleCtrls, SHDocVw, ActiveX, COMObj, MSHTML,
+  Htmlview, Readhtml, FramView, FramBrwz;
 
 type
   TMainForm = class(TForm)
@@ -26,7 +29,32 @@ type
     AbyssBashNavBarGroupControl: TdxNavBarGroupControl;
     AbyssBestBashNavBarGroupControl: TdxNavBarGroupControl;
     AbyssTopBashNavBarGroupControl: TdxNavBarGroupControl;
+    AntiFreeze: TIdAntiFreeze;
+    TestPageControl: TPageControl;
+    LogTabSheet: TTabSheet;
+    OtherTabSheet: TTabSheet;
+    btn1: TButton;
+    edt1: TEdit;
+    IdHTTP1: TIdHTTP;
+    WebTabControl: TTabControl;
+    wb1: TWebBrowser;
+    LogListBox: TListBox;
+    ConsoleInputEdit: TEdit;
+    MemoTabSheet: TTabSheet;
+    Memo1: TMemo;
+    ts1: TTabSheet;
+    htmlvwr1: THTMLViewer;
+    MainHtmlBrowser: THTMLViewer;
     procedure FormCreate(Sender: TObject);
+    procedure btn1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure BashTabSheetEnter(Sender: TObject);
+    procedure MainHtmlBrowserMouseWheel(Sender: TObject;
+      Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+      var Handled: Boolean);
+    procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure MainHtmlBrowserKeyPress(Sender: TObject; var Key: Char);
+    procedure LogListBoxKeyPress(Sender: TObject; var Key: Char);
   private
     { Private declarations }
   public
@@ -40,17 +68,147 @@ implementation
 
 {$R *.dfm}
 
+// Запись лога
+procedure WriteLog(Str: string);
+begin
+  Str := '[' + TimeToStr(Time) + ']  ' + Str;
+  MainForm.LogListBox.AddItem(Str,MainForm);
+end;
+
+// Открытие-закрытие тестаба
+procedure OpenCloseTestTab;
+begin
+  if MainForm.TestTabSheet.TabVisible = False
+  then begin
+          WriteLog('TestTab закрыт - открываем и переходим на лог');
+          MainForm.TestTabSheet.TabVisible := True;
+          WriteLog('Переходим на TestTab.');
+          // Реализовать переход на тесттаб
+          WriteLog('Переходим на LogTab.');
+          MainForm.TestPageControl.TabIndex := 0;
+       end
+  else begin
+          WriteLog('TestTab открыт - закрываем и переходим на главную баша');
+          MainForm.TestTabSheet.TabVisible := False;
+       end;
+end;
+
+// Обработка горячих клавиш
+procedure HotKeyMaster(Key: Char);
+begin
+  WriteLog('HotKeyMaster: нажата клавиша ' + Key);
+  case Key of
+    '`','ё': OpenCloseTestTab;
+  end;
+end;
+
+// Ищем окно для выставления фокуса, чтобы "не терять скроллинг"
+procedure FindFocus;
+begin
+  // Временный код
+  MainForm.MainHtmlBrowser.SetFocus;
+  WriteLog('Передача фокуса на Главную');
+end;
+
+// Переход на новую цитату
+procedure BashNext;
+begin
+  WriteLog('Конец цитаты');
+  ShowMessage('Конец. Нужно переходить на новую цитату');
+end;
+
+// Получаем html код страницы по указанной ссылке
+function GetStringFromUrl(url: string): string;
+begin
+  WriteLog('Получаем html код со страницы ' + url);
+  GetStringFromUrl := MainForm.IdHTTP1.Get(url);
+end;
+
+// Загружаем html главной в строку
+function BashGetMainAsString: string;
+begin
+  WriteLog('Загрузка html главной баша в строку');
+  BashGetMainAsString := GetStringFromUrl('http://bash.org.ru/');
+end;
+
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  WriteLog('Создание формы.');
   // Меняем размер формы на более компактный.
   MainForm.Width := 366;
   MainForm.Height := 450;
+  WriteLog('Меняем размер формы на ' + IntToStr(MainForm.Width) + 'x' + IntToStr(MainForm.Height));
 
-  // Переходим на BashTab
+  // Переводим PageControl'ы на начало.
+  WriteLog('Переходим на BashTab.');
   MainPageControl.TabIndex := 0;
+  WriteLog('Переходим на LogTab.');
+  TestPageControl.TabIndex := 0;
 
   // Убираем TestTab
   //TestTabSheet.TabVisible := False;
+
+  // Временный код
+  // Переводим строку в поток и выводим с форматированием
+  MainHtmlBrowser.LoadFromStream(TStringStream.Create(Memo1.Text));
+  //MainHtmlBrowser.LoadTextFromString(Memo1.Text);
+  WriteLog('Отображение тестовой цитаты');
+
 end;
+
+procedure TMainForm.btn1Click(Sender: TObject);
+var S:string;
+mm:TStream;
+begin
+  // Временный код
+  S:=Memo1.Text;
+  mm := TStringStream.Create(S);
+  htmlvwr1.LoadFromStream(mm);
+ 
+end;
+
+// Функции и процедуры, невыполнимые на этапе создания формы
+procedure TMainForm.FormShow(Sender: TObject);
+begin
+  FindFocus;
+end;
+
+procedure TMainForm.BashTabSheetEnter(Sender: TObject);
+begin
+  FindFocus;
+end;
+
+procedure TMainForm.MainHtmlBrowserMouseWheel(Sender: TObject;
+  Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+  // Переход на новую цитату при прокрутке колесом мыши
+  if ((MainHtmlBrowser.VScrollBarPosition = MainHtmlBrowser.VScrollBarRange) or (MainHtmlBrowser.VScrollBarRange < 0)) and (WheelDelta < 0)
+  then BashNext;
+end;
+
+/////////////////////////////////////////////////////////////////
+// Блок перехвата клавиш                                       //
+/////////////////////////////////////////////////////////////////
+
+procedure TMainForm.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  HotKeyMaster(Key);
+end;
+
+procedure TMainForm.MainHtmlBrowserKeyPress(Sender: TObject;
+  var Key: Char);
+begin
+  HotKeyMaster(Key);
+end;
+
+procedure TMainForm.LogListBoxKeyPress(Sender: TObject; var Key: Char);
+begin
+  HotKeyMaster(Key);
+end;
+
+/////////////////////////////////////////////////////////////////
+// Конец блока перехвата клавиш                                //
+/////////////////////////////////////////////////////////////////
 
 end.
